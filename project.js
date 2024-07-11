b9516abd0e16c47ffe2118a012d272f18bb7167e
Shohruh2807 <jalilovshohruh2010@gmail.com>
2024-07-11 10:03:56 +0500

    7a---7c commit

---
+++ project.js
@@ -0,0 +1,12 @@
+'use strict'
+let name=prompt('What is your name?')
+function salomlashish(){
+    if (!name){
+        console.log('Salom hammaga!')
+    }
+    else{
+    console.log(`Salom!${name}`)}
+}
+salomlashish();
+salomlashish();
+salomlashish();

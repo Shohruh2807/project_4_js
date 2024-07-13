875ca1714dd7b5a0da83d356520ac4393570958f
Jalilov Shohruh <138957134+Shohruh2807@users.noreply.github.com>
2024-07-13 10:50:22 +0500

    Update project2.js
    errors have been corrected

--- project2.js
+++ project2.js
@@ -9,12 +9,11 @@ function convertToCelsius(farengeyt){
 convertToCelsius(86)
 function temperaturaniOzgartirish(raqam, gradus) {
     if (gradus==='C'){
-        console.log(raqam*9/5+32+'F')
+        return raqam*9/5+32+'F'
     }
     else{
-        console.log((raqam-32)*5/9+'C')
+        return (raqam-32)*5/9+'C'
     }
-    return gradus
 }
 temperaturaniOzgartirish(25,'C')
 temperaturaniOzgartirish(86,'F')
